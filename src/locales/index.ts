06f8cc2e6675f2e8feeca72d725fba393ce848cb
Camille HBP <11173924+CamilleHbp@users.noreply.github.com>
2022-04-22 09:00:03 +0200

    chore(locales): enum uses uppercase

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,4 +1,4 @@
 export { messages as deMessages } from './de/messages';
 export { messages as enMessages } from './en/messages';
 export { messages as frMessages } from './fr/messages';
-export * from './locales';
+export * from './Locales';
