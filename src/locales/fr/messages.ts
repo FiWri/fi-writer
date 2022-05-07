6501a3d7f33bc78cc53f7d0ac2d377991ebca7fc
Camille HBP <11173924+CamilleHbp@users.noreply.github.com>
2022-05-07 12:28:56 +0200

    setup: add lint-staged and husky

--- src/locales/fr/messages.ts
+++ src/locales/fr/messages.ts
@@ -1,1 +1,17 @@
-/*eslint-disable*/export const messages={"404-not-found":"The page you're trying to reach does not exist. Sorry about that!","A PWA made for planning and writing interactive fictions!":"Une application PWA pour plannifier et ecrire des histoires interactives !","Click me":"Clique moi","Close me":"Ferme moi","FiWri":"FiWri","Github":"Github","home.app-description":"A PWA made for planning and writing interactive fictions!","modal.close":"Close","open modal":"open modal","switch color mode":"Changer de mode jour/nuit","switch language":"Changer la langue","switch theme":"Changer de thème"};
+/*eslint-disable*/ export const messages = {
+  '404-not-found':
+    "The page you're trying to reach does not exist. Sorry about that!",
+  'A PWA made for planning and writing interactive fictions!':
+    'Une application PWA pour plannifier et ecrire des histoires interactives !',
+  'Click me': 'Clique moi',
+  'Close me': 'Ferme moi',
+  FiWri: 'FiWri',
+  Github: 'Github',
+  'home.app-description':
+    'A PWA made for planning and writing interactive fictions!',
+  'modal.close': 'Close',
+  'open modal': 'open modal',
+  'switch color mode': 'Changer de mode jour/nuit',
+  'switch language': 'Changer la langue',
+  'switch theme': 'Changer de thème',
+};
