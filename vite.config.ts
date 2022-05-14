aa3f4c0f111841f14e76a3933e67f1004aa78331
Camille HBP <11173924+CamilleHbp@users.noreply.github.com>
2022-05-14 09:45:22 +0200

    fix: remove https

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import * as path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -14,10 +13,8 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-    https: true,
   },
   plugins: [
-    mkcert(),
     react({
       babel: {
         // Use .babelrc config file
