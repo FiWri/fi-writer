3d26d0d1f757d610a07078b611d54288de8ebb52
PaulPEHARPRE <paul.peharpre@koji.fr>
2022-05-20 12:20:30 +0200

    feat(modal-context): a context wich open into a portal

--- src/components/simpleModal.tsx
+++ src/components/simpleModal.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro';
 import { ReactNode } from 'react';
-import { Box, BoxProps, Button, Flex, Heading } from 'theme-ui';
+import * as ReactDom from 'react-dom';
+import { Box, BoxProps, Button, Flex, Heading, FlexProps } from 'theme-ui';
 
 import { useSimpleModal } from '~/hooks';
 
@@ -9,17 +10,38 @@ import { useSimpleModal } from '~/hooks';
 interface StyledModalProps extends BoxProps {
   isOpen: boolean;
 }
+
+const Overlay = (props: FlexProps) => (
+  <Flex
+    sx={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0, 0, 0, .7)',
+    }}
+  >
+    {props.children}
+  </Flex>
+);
+
 const StyledModal = ({ isOpen, ...props }: StyledModalProps) => (
   <Box
     sx={{
       border: 'thick',
       borderRadius: 'radius3',
-      bg: 'background',
+      bg: 'white',
       color: 'text',
+      position: 'fixed',
+      alignItems: 'center',
       flexDirection: 'column',
       justifyContent: 'space-around',
       padding: '1em 1em',
       width: '60vw',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
     }}
   >
     {props.children}
@@ -45,25 +67,30 @@ interface SimpleModalProps {
   title?: string;
   children?: ReactNode;
 }
+const portalDiv: HTMLElement = document.getElementById('portal')!;
 
 export const SimpleModal = ({ children, title }: SimpleModalProps) => {
   const { isOpen, closeModal } = useSimpleModal();
   console.log(isOpen);
   if (isOpen) {
-    return (
-      <StyledModal
-        // onBackgroundClick={closeModal}
-        // onEscapeKeydown={closeModal}
-        isOpen={isOpen}
-      >
-        {title && <Heading as="h2">{title}</Heading>}
-        <Content>{children}</Content>
-        <Flex sx={{ justifyContent: 'center', margin: '1em' }}>
-          <Button onClick={closeModal}>
-            <Trans id="modal.close">Close</Trans>
-          </Button>
-        </Flex>
-      </StyledModal>
+    return ReactDom.createPortal(
+      <>
+        <Overlay />
+        <StyledModal
+          // onBackgroundClick={closeModal}
+          // onEscapeKeydown={closeModal}
+          isOpen={isOpen}
+        >
+          {title && <Heading as="h2">{title}</Heading>}
+          <Content>{children}</Content>
+          <Flex sx={{ justifyContent: 'center', margin: '1em' }}>
+            <Button onClick={closeModal}>
+              <Trans id="modal.close">Close</Trans>
+            </Button>
+          </Flex>
+        </StyledModal>
+      </>,
+      portalDiv
     );
   }
   return <></>;
